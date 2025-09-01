604971b4de5559a264a5abc4ce62a327bf781138
kirjavascript <snkenjoi@gmail.com>
2025-09-01 19:35:47 +0100

    bugfix

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod data;
 // 2F for / 23 for #
-// TODO: add data padding,
+// 475 prepadding 505 equal
 
 fn main(){
     let a=data::DATA; // __START_DATA__";
@@ -17,6 +17,6 @@ fn main(){
 
     let mut f=g.iter().map(|r|r.iter().collect::<String>()).collect::<Vec<_>>().join("\n");
     let v=a.chars().flat_map(|c|(c>' ').then(||char::from_u32((c as u32)-200).unwrap())).collect::<String>();
-    for c in format!(" fn main(){{let a=\"{a:<470}{v}").chars(){f=f.replacen(b,&c.to_string(),1)}
+    for c in format!(" fn main(){{let a=\"{a:<505}{v}").chars(){f=f.replacen(b,&c.to_string(),1)}
     println!("{f}");
 }
